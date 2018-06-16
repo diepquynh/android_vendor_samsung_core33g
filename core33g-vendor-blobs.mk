d52c76bf983b19008cd349d8ec4bc6c8740eb123
Remilia Scarlet <remilia.1505@gmail.com>
2018-06-16 05:53:54 +0000

    core33g: Rename prebuilt sensors HAL to use wrapper
    Change-Id: Ib2887664d5ebc2fcc816805657a0df266db77322
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- core33g-vendor-blobs.mk
+++ core33g-vendor-blobs.mk
@@ -27,4 +27,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/core33g/proprietary/lib/libmorpho_easy_hdr.so:system/vendor/lib/libmorpho_easy_hdr.so \
     vendor/samsung/core33g/proprietary/lib/libuvdenoise.so:system/vendor/lib/libuvdenoise.so \
     vendor/samsung/core33g/proprietary/lib/hw/camera.sc8830.so:system/vendor/lib/hw/camera.sc8830.so \
-    vendor/samsung/core33g/proprietary/lib/hw/sensors.sc8830.so:system/vendor/lib/hw/sensors.sc8830.so \
+    vendor/samsung/core33g/proprietary/lib/hw/sensors.vendor.sc8830.so:system/vendor/lib/hw/sensors.vendor.sc8830.so \
